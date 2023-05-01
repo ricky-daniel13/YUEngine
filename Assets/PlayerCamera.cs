1df2b6bcd2295ba7522feb1f892c55d23ead1426
Ricardo Daniel Garibaldi Oliva <garibaldi.rdaniel@gmail.com>
2023-05-01 08:29:52 -0600

    Camera Progress
    Camera Progress

--- Assets/PlayerCamera.cs
+++ Assets/PlayerCamera.cs
@@ -4,28 +4,63 @@ using UnityEngine;
 
 public class PlayerCamera : MonoBehaviour
 {
-    public float cameraDecel, cameraDefaultLenght, currentCameraLength;
+    public float cameraAccel, cameraDefaultLenght, currentCameraLength;
     Vector3 velocity, currentPosition;
     public Transform cameraHolder;
     Quaternion currentRotation = Quaternion.LookRotation((Vector3.back+Vector3.up*0.15f).normalized);
     public PlayerAdventure player;
     Vector3 lastPlayerPos, playerVelocity;
+
+    public Vector3 vecDefaultCameraRota;
+    Quaternion defaultCameraRota;
     // Start is called before the first frame update
     void Start()
     {
-        currentPosition = player.transform.position + Vector3.up * 0.8f;
+        currentPosition = (player.transform.position + Vector3.up * 0.8f) + (currentRotation * new Vector3(0, 0, cameraDefaultLenght));
         lastPlayerPos = player.transform.position;
+        defaultCameraRota = Quaternion.Euler(vecDefaultCameraRota);
     }
 
     // Update is called once per frame
     void LateUpdate()
     {
-        currentCameraLength = cameraDefaultLenght;
         playerVelocity = (player.transform.position - lastPlayerPos)/Time.deltaTime;
+        Vector3 correctedPlayerPos =  player.transform.position + player.player.GetGroundNormal * 0.8f;
+        Vector3 localPlayerPos = player.transform.position - currentPosition;
+        Vector3 localCameraPos = currentPosition-player.transform.position;
+        velocity = Vector3.Project(playerVelocity, Extensions.ProjectDirectionOnPlane(cameraHolder.forward, -player.player.gravityDir));
+        velocity += Vector3.Project(playerVelocity, -player.player.gravityDir);
+        Vector3 rgtAxis = Vector3.Cross(Extensions.ProjectDirectionOnPlane(cameraHolder.forward, -player.player.gravityDir), -player.player.gravityDir);
 
+        Vector3 cameraDirection = currentPosition - player.transform.position;
+        //Debug.Log("Camera distance= " + cameraDirection.magnitude);
+        if(cameraDirection.sqrMagnitude > cameraDefaultLenght * cameraDefaultLenght){
+            velocity += -(cameraDirection.normalized) * (10f + Time.deltaTime);
+        }
 
-        velocity = Vector3.Project(playerVelocity, Extensions.ProjectDirectionOnPlane(cameraHolder.forward, player.player.GetGroundNormal));
-        velocity += Vector3.Project(playerVelocity, player.player.GetGroundNormal);
+        float cameraHeightToSonic = Vector3.Dot(localCameraPos, player.player.GetGroundNormal);
+        if((cameraHeightToSonic-0.8f) > Mathf.Epsilon)
+            velocity += player.player.GetGroundNormal * (Mathf.MoveTowards(cameraHeightToSonic, 0, 30));
+
+        Debug.Log("Camera height difference = " + cameraHeightToSonic);
+
+        Quaternion idToCurr = Quaternion.LookRotation(Extensions.ProjectDirectionOnPlane(cameraHolder.forward, -player.player.gravityDir), -player.player.gravityDir);
+        Vector3 desiredPosition = correctedPlayerPos  + Vector3.up * 0.8f + (idToCurr * defaultCameraRota * (Vector3.back * cameraDefaultLenght));
+        Debug.DrawRay(desiredPosition, Vector3.Cross(currentPosition - correctedPlayerPos , rgtAxis).normalized, Color.magenta);
+        Vector3 localDesired = desiredPosition - currentPosition;
+        float dirMult=1;
+        
+        if(Vector3.Dot(-player.player.gravityDir, localDesired)<0)
+            dirMult=-1;
+
+        if(Mathf.Abs(Vector3.Dot(-player.player.gravityDir, localDesired)) > 1f)
+            velocity += Vector3.Cross(desiredPosition - correctedPlayerPos, rgtAxis).normalized * (Mathf.Clamp01(localDesired.magnitude)*dirMult*cameraAccel);
+
+        
+
+        velocity += Vector3.Cross(currentPosition - player.transform.position, -player.player.gravityDir).normalized * (-Input.GetAxis("Mouse X")*cameraAccel);
+        //velocity = Vector3.Cross(transform.position - centerOfRotation.position, centerOfRotation.up).normalized * orbitSpeed;
+
         ProcessVelocity(Time.deltaTime);
         lastPlayerPos = player.transform.position;
 
@@ -34,7 +69,7 @@ public class PlayerCamera : MonoBehaviour
     void ProcessVelocity(float deltaTime)
     {
         currentPosition += (velocity * deltaTime);
-        cameraHolder.transform.position = currentPosition + (currentRotation * new Vector3(0, 0, currentCameraLength));
+        cameraHolder.transform.position = currentPosition;
         cameraHolder.LookAt(player.transform);
     }
 }
